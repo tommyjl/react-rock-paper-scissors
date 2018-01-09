c1ee0243cd8afc83dadaf81f211d396323199955
Tommy Joe Lund <tomlun92@gmail.com>
2018-01-09 09:38:52 +0100

    Organise imports

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
 import Title from "./Title";
 import Opponent from "../Player/Opponent";
 import Results from "./Results";
